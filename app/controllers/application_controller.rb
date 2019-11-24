218f473d145998c2c2b4b9d3f4bef05ac1431eb4
Manoj Sirsath <sirsathmanoj5@gmail.com>
2019-11-24 18:53:26 +0530

    added login page & css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+
+  # def user_is_logged_in
+  #   if !session[:current_user]
+  #     redirect_to '/users/sign_in'
+  #   end
+  # end
+
+  # def after_sign_in_path_for(resource)
+  #   stored_location_for(resource) || welcome_path
+  # end
+
+  def after_sign_in_path_for(resource)
+    # return the path based on resource
+    patients_path
+  end
+
+  # def after_sign_out_path_for(resource)
+  #   # return the path based on resource
+  #   redirect_to new_user_session
+  # end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+   devise_for :users, controllers: { registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords" }
   resources :patients
   resources :users
-  root  'patients#index'
+   devise_scope :user do
+     root to: "users/sessions#new"
+   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
